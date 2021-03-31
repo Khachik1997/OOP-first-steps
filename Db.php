1190988144d475cd42a5001b9f6eece487d6285a
unknown <you@example.com>
2021-03-31 17:11:09 +0400

    corect Db

--- Db.php
+++ Db.php
@@ -1,68 +1,89 @@
 <?php
-
 class Db
 {
-    private $mysqli;
-
+    private $conn;
+    private $whereStr ="";
     function __construct()
     {
-
-        $this->mysqli = new mysqli("localhost", "root", "", "oop_database");
-
-        if ($this->mysqli->connect_errno) {
-            echo "Failed to connect to MySQL: " . $this->mysqli->connect_error;
+        $this->conn = new mysqli("localhost", "root", "", "oop_database");
+        if ($this->conn->connect_errno) {
+            echo "Failed to connect to MySQL: " . $this->conn->connect_error;
             exit();
         }
-
     }
+
+
+    public function where($column,$value,$oper = '='){
+
+        if(($this->whereStr)){
+            $this->whereStr = $this->whereStr." AND $column $oper '$value' ";
+        }else{
+            $this->whereStr =" WHERE $column $oper '$value'";
+        }
+
+        return $this;
+    }
+    public function orWhere($column,$value,$oper = '='){
+
+        if(($this->whereStr)){
+            $this->whereStr = $this->whereStr." OR $column $oper '$value' ";
+        }else{
+            $this->whereStr =" WHERE $column $oper '$value'";
+        }
+
+        return $this;
+    }
+
 
     public function select($sql)
     {
-        $result = $this->mysqli->query($sql);
-        var_dump($result->fetch_all());
-
+        $arrResult = [];
+        $result = $this->conn->query($sql);
+        if($result->num_rows == 1){
+            return $result->fetch_assoc();
+        }else {
+            while($row = $result->fetch_assoc()) {
+                $arrResult[] = $row;
+            }
+            return $arrResult;
+        }
     }
 
     public function insert($tbl_name, $data)
     {
-        $name = $data['name'];
-        $surname = $data['surname'];
-        $email = $data['email'];
-        $sql = "SELECT email FROM user WHERE email ='$email' ";
-        $duplicate = $this->mysqli->query($sql);
-        if($duplicate->num_rows > 0){
-            return "That email is already in use";
-        }else{
-            $sql = "INSERT INTO $tbl_name (name,surname,email) VALUES ('$name','$surname','$email') ";
-            if ($this->mysqli->query($sql) === TRUE) {
-                return "New record created successfully";
-            } else {
-                return "Error: " . $sql . "<br>" . $this->mysqli->error;
-            }
-        }
-
+        $sql = "INSERT INTO $tbl_name (".implode(",",array_keys($data)) . ") VALUES ('".implode("','",array_values($data)) ."')"  ;
+        return $this->conn->query($sql);
     }
 
-    public function update($table_name, $data, $id)
+    public function update($table_name, $data)
     {
-        $name = $data['name'];
-        $surname = $data['surname'];
-        $email = $data['email'];
-        $sql = "UPDATE $table_name Set  name = '$name',surname = '$surname',email = '$email' WHERE id = '$id'";
-        if ($this->mysqli->query($sql) === TRUE) {
+        $where_condition =$this->whereStr;
+        $this->whereStr = "";
+        $updateData="";
+        foreach ($data as $key => $value){
+            $updateData .=$key. "= '".$value."' , ";
+
+        }
+        $updateData = substr($updateData,0,-2);
+        $sql = "UPDATE  $table_name  SET   $updateData $where_condition";
+
+        if ($this->conn->query($sql) === TRUE) {
             return "New record created successfully";
         } else {
-            return "Error: " . $sql . "<br>" . $this->mysqli->error;
+            return "Error: " . $sql . "<br>" . $this->conn->error;
         }
     }
 
-    public function delete($table_name, $id)
+    public function delete($table_name)
     {
-        $sql = "DELETE FROM $table_name WHERE  id ='$id'";
-        if ($this->mysqli->query($sql) === TRUE) {
+        $where_condition = $this->whereStr;
+        $this->whereStr = "";
+
+        $sql = "DELETE FROM  $table_name   $where_condition";
+        if ($this->conn->query($sql) === TRUE) {
             return "New record created successfully";
         } else {
-            return "Error: " . $sql . "<br>" . $this->mysqli->error;
+            return "Error: " . $sql . "<br>" . $this->conn->error;
         }
     }
 
@@ -70,11 +91,3 @@ class Db
 
 
 $db = new Db;
-//$arrResultData = $db->select("SELECT * FROM user");
-$data = ["name" => "GAG", "surname" => "Smite", "email" => "John@.com"];
-$boolResultSuccess = $db->insert("user", $data);
-
-
-//$boolResultUpdate = $db->update('user', $data, 18);
-//$boolResultDelete = $db->delete('user', "18");
-echo $boolResultSuccess;
