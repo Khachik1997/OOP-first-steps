7ecf2d8b5b9153e0cbf9ddf4c0a00cf75af0fabf
unknown <you@example.com>
2021-04-01 22:22:28 +0400

    chanege conditions

--- Db.php
+++ Db.php
@@ -2,7 +2,8 @@
 class Db
 {
     private $conn;
-    private $whereStr ="";
+    private $whereStr ;
+
     function __construct()
     {
         $this->conn = new mysqli("localhost", "root", "", "oop_database");
@@ -14,23 +15,19 @@ class Db
 
 
     public function where($column,$value,$oper = '='){
-
         if(($this->whereStr)){
             $this->whereStr = $this->whereStr." AND $column $oper '$value' ";
         }else{
             $this->whereStr =" WHERE $column $oper '$value'";
         }
-
         return $this;
     }
     public function orWhere($column,$value,$oper = '='){
-
         if(($this->whereStr)){
             $this->whereStr = $this->whereStr." OR $column $oper '$value' ";
         }else{
             $this->whereStr =" WHERE $column $oper '$value'";
         }
-
         return $this;
     }
 
@@ -51,43 +48,37 @@ class Db
 
     public function insert($tbl_name, $data)
     {
-        $sql = "INSERT INTO $tbl_name (".implode(",",array_keys($data)) . ") VALUES ('".implode("','",array_values($data)) ."')"  ;
+        $dataVal = "";
+        foreach ($data as $key => $value){
+            $dataVal.=  "'" .$this->conn->real_escape_string($value) . "',";
+        }
+        $dataVal = substr($dataVal,0,-1);
+        $sql = "INSERT INTO $tbl_name (".implode(",",array_keys($data)) . ") VALUES ($dataVal)"  ;
         return $this->conn->query($sql);
     }
-
     public function update($table_name, $data)
     {
-        $where_condition =$this->whereStr;
+        $where_condition = $this->whereStr;
         $this->whereStr = "";
         $updateData="";
         foreach ($data as $key => $value){
-            $updateData .=$key. "= '".$value."' , ";
-
+            $updateData .=$key. "= '".$this->conn->real_escape_string($value) ."',";
         }
         $updateData = substr($updateData,0,-2);
         $sql = "UPDATE  $table_name  SET   $updateData $where_condition";
-
-        if ($this->conn->query($sql) === TRUE) {
-            return "New record created successfully";
-        } else {
-            return "Error: " . $sql . "<br>" . $this->conn->error;
-        }
+        return $this->conn->query($sql);
     }
 
     public function delete($table_name)
     {
         $where_condition = $this->whereStr;
         $this->whereStr = "";
-
         $sql = "DELETE FROM  $table_name   $where_condition";
-        if ($this->conn->query($sql) === TRUE) {
-            return "New record created successfully";
-        } else {
-            return "Error: " . $sql . "<br>" . $this->conn->error;
-        }
+        return $this->conn->query($sql);
     }
 
 }
 
 
 $db = new Db;
+
